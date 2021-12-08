f032777b3c38d3ad17ad3457bc02faec742e6c6a
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2021-12-08 22:00:40 +0330

    added an infinite loop and a condition to break if user win

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,22 +6,25 @@ fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..101); // to get a number between 1-100 we also can use 1..=100
     println!("The secret numbere is {}", secret_number);
-    println!("Please Enter your guess.");
+    loop {
+        let mut guess = String::new(); // in Rust variables are immutable by default; use "mut" to make them mutable
+                                       // "::" indicates that "new" is an associated function of "String" type
+        println!("Please Enter your guess.");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Field to read line");
 
-    let mut guess = String::new(); // in Rust variables are immutable by default; use "mut" to make them mutable
-                                   // "::" indicates that "new" is an associated function of "String" type
+        let guess: u32 = guess.trim().parse().expect("Please type a number!"); // change guess type from String to u32
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Field to read line");
+        println!("You guess: {}", guess);
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!"); // change guess type from String to u32
-
-    println!("You guess: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too Small!"),
-        Ordering::Equal => println!("You Win!"),
-        Ordering::Greater => println!("Too Big!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Equal => {
+                println!("You Win!");
+                break;
+            }
+            Ordering::Greater => println!("Too Big!"),
+        }
     }
 }
