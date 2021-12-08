168c312796f117f32574d9f52c1c5c3d5927dcaf
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2021-12-08 22:08:03 +0330

    the game is ready!

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,7 +5,7 @@ use std::io;
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..101); // to get a number between 1-100 we also can use 1..=100
-    println!("The secret numbere is {}", secret_number);
+
     loop {
         let mut guess = String::new(); // in Rust variables are immutable by default; use "mut" to make them mutable
                                        // "::" indicates that "new" is an associated function of "String" type
@@ -14,7 +14,13 @@ fn main() {
             .read_line(&mut guess)
             .expect("Field to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!"); // change guess type from String to u32
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num, //return the converted value if everything be Ok
+            Err(_) => {
+                println!("Please Enter a number...");
+                continue;
+            } //show a line and continue the app if an Err ocurred
+        }; // change guess type from String to u32
 
         println!("You guess: {}", guess);
 
