2a52dda9889a39a5e9c69d1c19f07cbacaa0e8da
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2021-12-08 21:24:24 +0330

    added random number generator

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,10 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..101); // to get a number between 1-100 we also can use 1..=100
+    println!("The secret numbere is {}", secret_number);
     println!("Please Enter your guess.");
 
     let mut guess = String::new(); // in Rust variables are immutable by default; use "mut" to make them mutable
