f5e8d46a6af5af2de7dd6f44f24efe48cc44a1a1
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2021-12-09 10:40:09 +0330

    using const

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,14 @@
+//type of const should be declared
+const THREE_HOURS_IN_SECONS: u32 = 3 * 60 * 60;
+
 fn main() {
     let mut x = 5;
     println!("The value of x is {}", x);
     // if we don't use "mut", we can't change x now
     x = 6;
     println!("The value of x is {}", x);
+    println!(
+        "The value of THREE_HOURS_IN_SECONS is {}",
+        THREE_HOURS_IN_SECONS
+    );
 }
