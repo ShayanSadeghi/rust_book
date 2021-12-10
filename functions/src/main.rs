d17e03f919e9b6382dc4e2c49bbc3731018356db
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2021-12-10 15:56:01 +0330

    using a fuction which returns a value

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,10 @@
 fn main() {
     println!("Hello, world!");
 
-    time_message(5, 'h');
+    let x = plus_one(5);
+    println!("The value is {}", x)
 }
 
-fn time_message(x: i32, label: char) {
-    println!("The time is {}{}", x, label);
+fn plus_one(x: i32) -> i32 {
+    x + 1 //it is an expression not a statement, so we should not to use ';' at the end
 }
