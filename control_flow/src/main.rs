1091d56a96e6b3dc09a3c3080a9dfbf919902b77
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2021-12-11 22:49:00 +0330

    using infinite loop and label on a loop

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -2,6 +2,7 @@ fn main() {
    // if - else
    let x: i8 = 6;
 
+   // conditions explicitly should be boolean not numbers etc...
    if x % 4 == 0 {
       println!("Number is divisible by 4");
    } else if x % 3 == 0 {
@@ -16,4 +17,25 @@ fn main() {
    let number = if condition { 5 } else { 2 }; // values in each arm (if-else) should be in a same type
                                                //and blocks should be expression not statement
    println!("The number is {}", number);
+
+   // Repetition
+   // "loop" run until we break
+   let mut count = 0;
+   'counting_up: loop {
+      // add label for loop
+      println!("count = {}", count);
+      let mut remaining = 10;
+      loop {
+         println!("remaining = {}", remaining);
+         if remaining == 9 {
+            break; //break inner loop
+         }
+         if count == 2 {
+            break 'counting_up; //break outer loop (with label counting_loop)
+         }
+         remaining -= 1;
+      }
+      count += 1;
+   }
+   println!("End count = {}", count);
 }
