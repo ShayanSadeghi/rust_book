095758f5191407aac1f63cab5f96b14519ce295e
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2021-12-14 11:58:24 +0330

    use mutalbe reference after immutable reference

--- ownership/refrences/src/main.rs
+++ ownership/refrences/src/main.rs
@@ -1,7 +1,10 @@
 fn main() {
     let mut s1 = String::from("Hello"); //define a mutable variable
+
     let r1 = &s1; //we can have multiple immutable referencing
     let r2 = &s1;
-    // after immutable references we can't define a new mutable reference
     println!("values are {}, {}", r1, r2);
+    // because out immutable references will not be used after this point, we can create a mutable reference without any error
+    let r3 = &mut s1;
+    println!("values are {}", r3);
 }
