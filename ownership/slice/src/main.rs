b904de0208379c815f8550057c7c59d929b63b34
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2021-12-18 21:13:14 +0330

    do a simple change to get both String and literal in first_word function

--- ownership/slice/src/main.rs
+++ ownership/slice/src/main.rs
@@ -1,23 +1,23 @@
 fn main() {
-  let mut s = String::from("hello world");
+  let s = String::from("hello world");
   let word = first_word(&s);
-  s.clear(); // we clear s and so we shouldn't have access to word after this line
 
-  // if we use word, which is an immutable variable after mutating the refrence string, s, (using clear) we get an error during the compile time and it is usefull :)
+  println!("first word of our String variable is {}", word);
 
-  println!("{}", word);
-
-
+  let s_literal = "hello world";
+  let word = first_word(&s_literal);
+  
+  println!("first word of our literal variable is {}", word);
 }
 
-fn first_word(s: &String) -> &str{
+fn first_word(s: &str) -> &str{
   //return the index of the first word.
   let bytes = s.as_bytes();
   
   for (i, &item) in bytes.iter().enumerate(){
     if item == b' '{
 	
-      return &s[..i]; // it is same as &s[0..i
+      return &s[..i]; // it is same as &s[0..i]
     }
   
   }
