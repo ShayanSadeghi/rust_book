5657ef799c4163f0a4c2dea6ecda9daed7ac1479
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2021-12-18 21:06:25 +0330

    using string slicing to use borrowing concept in error finding

--- ownership/slice/src/main.rs
+++ ownership/slice/src/main.rs
@@ -1,21 +1,25 @@
 fn main() {
   let mut s = String::from("hello world");
   let word = first_word(&s);
-  s.clear(); // when we clear the actual string, index stored in "word" variable isn't usefull but we keep it and it is error prone.
+  s.clear(); // we clear s and so we shouldn't have access to word after this line
+
+  // if we use word, which is an immutable variable after mutating the refrence string, s, (using clear) we get an error during the compile time and it is usefull :)
+
+  println!("{}", word);
 
 
 }
 
-fn first_word(s: &String) ->usize{
+fn first_word(s: &String) -> &str{
   //return the index of the first word.
   let bytes = s.as_bytes();
   
   for (i, &item) in bytes.iter().enumerate(){
     if item == b' '{
 	
-      return i;
+      return &s[..i]; // it is same as &s[0..i
     }
   
   }
-  s.len()
+  &s[..]
 }
