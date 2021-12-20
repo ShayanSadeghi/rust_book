4289120afebb78f451bc0a63abf1c43431502f3e
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2021-12-20 20:21:39 +0330

    borrow struct

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -15,9 +15,20 @@ fn main() {
   };
   
   user1.email = String::from("another@mail.com");
+  
 
-  println!("{}", user1.email);
-  println!("{}", user1.active);
+  let user2 = User{
+    sign_in_count: 0,
+    ..user1 // user1 won't be usable after this, because borrowing email and username
+  };
+
+  let user3 = User{
+    email: String::from("user3@mail.com"),
+    username: String::from("user3"),
+    ..user2 // user2 will be usable because email and value entered so we don't move them to user3, other values will be copied
+  };
+  
+  println!("{}", user2.email);
 }
 
 
