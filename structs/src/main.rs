7ea7b0566814fc232770933c0ff2f8abd79232d4
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2021-12-20 20:08:25 +0330

    use shorthand syntax to define syntax

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,16 +1,23 @@
 fn main() {
-    // define a new struct type
-    struct User {
-        active: bool,
-        username: String,
-        email: String,
-        sign_in_count: u64,
-    }
 
-    let user1 = User {
-        email: String::from("user@example.com"),
-        username: String::from("someone_username"),
-        active: true,
-        sign_in_count: 1,
-    };
+  let user1 = build_user(String::from("some@mail.com"), String::from("user1234"));
+  println!("{}", user1.email);
+  println!("{}", user1.active);
+}
+
+
+struct User {
+  email: String,
+  username: String,
+  active: bool,
+  sign_in_count: u64,  
+}
+
+fn build_user(email: String, username: String) -> User{
+  User {
+    email, //use shorthand syntax
+    username, //use shorthand syntax
+    active: true,
+    sign_in_count:1,
+  }
 }
