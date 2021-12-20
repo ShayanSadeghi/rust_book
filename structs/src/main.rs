579d9781afde9ce88f88a82bcb9dd50a17178420
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2021-12-20 20:15:08 +0330

    use mutable structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,23 +1,23 @@
 fn main() {
 
-  let user1 = build_user(String::from("some@mail.com"), String::from("user1234"));
+  struct User {
+    email: String,
+    username: String,
+    active: bool,
+    sign_in_count: u64,  
+  }
+  
+  let mut user1 = User {
+    email: String::from("some@mail.com"),
+    username: String::from("some_user"),
+    active: true,
+    sign_in_count:1,
+  };
+  
+  user1.email = String::from("another@mail.com");
+
   println!("{}", user1.email);
   println!("{}", user1.active);
 }
 
 
-struct User {
-  email: String,
-  username: String,
-  active: bool,
-  sign_in_count: u64,  
-}
-
-fn build_user(email: String, username: String) -> User{
-  User {
-    email, //use shorthand syntax
-    username, //use shorthand syntax
-    active: true,
-    sign_in_count:1,
-  }
-}
