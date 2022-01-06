38864cdb3995efe6a0f4f36ccf28d9dd635813c1
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-01-07 00:24:04 +0330

    multi methods in one impl

--- structs_method/src/main.rs
+++ structs_method/src/main.rs
@@ -8,6 +8,10 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        // it is better to define each method in a separate imple
+        self.width >= other.width && self.height >= other.height
+    }
 }
 
 impl Rectangle {
@@ -21,6 +25,15 @@ fn main() {
         width: 10,
         height: 5,
     };
+    let rect2 = Rectangle {
+        width: 3,
+        height: 4,
+    };
+
+    let rect3 = Rectangle {
+        width: 12,
+        height: 6,
+    };
 
     println!(
         "The area of the rectangle is {} square pixels",
@@ -29,4 +42,7 @@ fn main() {
 
     println!("The width of the rectangle is {} pixels", rect1.width);
     println!("The width is greater than zero? {}", rect1.width());
+
+    println!("can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
