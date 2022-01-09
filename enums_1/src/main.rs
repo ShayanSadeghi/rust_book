93ec5c05c9772f6a8f64d0a117a2327d6de94308
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-01-09 22:29:28 +0330

    Use Option<T> for None values

--- enums_1/src/main.rs
+++ enums_1/src/main.rs
@@ -16,4 +16,10 @@ impl Message {
 fn main() {
     let my_message = Message::Write(String::from("Hello There!"));
     my_message.call();
+
+    // Using Option<T>
+    let some_number = Some(5); //some i32 --> Option<i32>
+    let some_string = Some("a string"); // some &str --> Option<&str>
+
+    let absent_number: Option<i32> = None; // None but it is i32
 }
