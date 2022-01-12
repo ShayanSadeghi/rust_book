d2c9c4f7c1ed53227546cbd074c83e935f3a9e68
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-01-13 00:13:25 +0330

    use if-let-else

--- if_let/src/main.rs
+++ if_let/src/main.rs
@@ -1,4 +1,20 @@
 // section 6.3
+#[derive(Debug)]
+enum UsState {
+    Alaska,
+    Florida,
+    Alabama,
+    Arizona,
+    Califonia,
+}
+
+enum Coin {
+    Penny,
+    Nickle,
+    Dime,
+    Quarter(UsState),
+}
+
 fn main() {
     // let config_max = Some(3u8);
     // match config_max {
@@ -15,4 +31,22 @@ fn main() {
 
     // use if-let when you want to do anything with the other match arms.
     // if-let don't consider all possibilities so it is Not Exhaustive
+
+    //Another match is like this:
+
+    let coin = Coin::Quarter(UsState::Califonia);
+    let mut count = 0;
+    // match coin {
+    //     Coin::Quarter(state) => println!("State quarter from {:?}", state),
+    //     _ => count += 1,
+    // }
+
+    // using if-let-else:
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}", state);
+    } else {
+        count += 1;
+    }
+
+    println!("non-quarter coin count: {}", count)
 }
