7c309e878a06f383dddcd6b084fb35e6e3a31b75
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-01-13 23:46:23 +0330

    create first module structure

---
+++ restaurant/src/lib.rs
@@ -0,0 +1,30 @@
+// section 7.2
+// use cargo new --lib "name" command
+
+mod front_of_house {
+    mod hosting {
+        fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
+}
+
+// tree:
+
+// crate
+//  └── front_of_house
+//  ├── hosting
+//  │   ├── add_to_waitlist
+//  │   └── seat_at_table
+//  └── serving
+//      ├── take_order
+//      ├── serve_order
+//      └── take_payment
