ef95d3a1c797515989db1faaa4a71165c1350039
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-01-15 16:14:15 +0330

    use nested path

--- 07/use_keyword/src/main.rs
+++ 07/use_keyword/src/main.rs
@@ -1,6 +1,10 @@
 // use std::io::Result as IoResult
-mod lib;
+// mod lib;
+use std::io::{self, Write}
+// the above use, import: std::io and std::io::Write
+
+
 fn main() {
-    lib::eat_at_restaurant();
-    lib::hosting::add_to_waitlist(); // it is because of re-exporting
+    // lib::eat_at_restaurant();
+    // lib::hosting::add_to_waitlist(); // it is because of re-exporting
 }
