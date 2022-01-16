0a0ecce1e0b360ccb864b9f36d7f666278979850
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-01-16 16:57:39 +0330

    reading vector elements

--- 08/vectors/src/main.rs
+++ 08/vectors/src/main.rs
@@ -15,4 +15,18 @@ fn main() {
         // all values pushed to new vector are in a same type, so rust can determine the type
         println!("{:?}", v_3);
     } // v3 is no longer accessible here
+
+    let v_2_1: &f32 = &v_2[1]; // if there is no element with index 1, the program will panic
+    println!("{}", v_2_1);
+
+    match v_2.get(1) {
+        Some(val) => println!("The second element is {}", val),
+        None => println!("There is no value with this index"),
+    }
+
+    let mut v_4 = vec![1, 2, 3, 4, 5];
+    // the next to lines cause an error together,
+    let first = &v_4[0];
+    // v_4.push(6);
+    println!("The first element is {}", first)
 }
