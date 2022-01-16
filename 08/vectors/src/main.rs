d556666e5c9a5388be2e796a83f138677224f806
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-01-16 16:42:58 +0330

    creating a vector

---
+++ 08/vectors/src/main.rs
@@ -0,0 +1,9 @@
+// section 8.1
+// Vectors
+fn main() {
+    let v_1: Vec<i32> = Vec::new();
+    println!("{:?}", v_1);
+
+    let v_2 = vec![1.0, 2.2, 3.1]; //use "vec!" can determine type of elements in vector automatically
+    println!("{:?}", v_2);
+}
