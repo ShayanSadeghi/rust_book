12062fc81ea2007b5abb8d00e9431cc420bcd14a
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-01-16 16:47:44 +0330

    update and drop a vector

--- 08/vectors/src/main.rs
+++ 08/vectors/src/main.rs
@@ -6,4 +6,13 @@ fn main() {
 
     let v_2 = vec![1.0, 2.2, 3.1]; //use "vec!" can determine type of elements in vector automatically
     println!("{:?}", v_2);
+    {
+        let mut v_3 = Vec::new();
+
+        v_3.push(1);
+        v_3.push(6);
+        v_3.push(3);
+        // all values pushed to new vector are in a same type, so rust can determine the type
+        println!("{:?}", v_3);
+    } // v3 is no longer accessible here
 }
