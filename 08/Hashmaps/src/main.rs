af29f4d07a442ded565c5c11967ceca548ba6b50
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-01-18 19:08:02 +0330

    HashMap takes the ownerships

--- 08/Hashmaps/src/main.rs
+++ 08/Hashmaps/src/main.rs
@@ -14,4 +14,18 @@ fn main() {
 
     let scores: HashMap<_, _> = teams.into_iter().zip(initial_scores.into_iter()).collect();
     println!("{:?}", scores);
+
+    // ownership
+    let field_name = String::from("favorite_color");
+    let field_value = String::from("red");
+    let mut m = HashMap::new();
+    m.insert(field_name, field_value); // field_name and field_value are not available now
+    println!("{:#?}", m);
+
+    let field_name_2 = String::from("Age");
+    let field_value_2 = 25;
+    let mut m2 = HashMap::new();
+    m2.insert(field_name_2, field_value_2); // int will be copied, so we can use it later
+    println!("{:#?}", m2);
+    println!("{}", field_value_2);
 }
