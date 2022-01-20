55ec154eed69ee73e133b81fa5b2db6ff01e6d53
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-01-20 21:01:29 +0330

    handle different errors kind

--- 09/recoverable_errors/src/main.rs
+++ 09/recoverable_errors/src/main.rs
@@ -1,9 +1,21 @@
 use std::fs::File;
+use std::io::ErrorKind;
 
 fn main() {
     let f = File::open("test.txt"); // if file doesn't exist, there is no default panic. it is a recoverable error
     let f = match f {
         Ok(file) => file, // return file if it exists in root directory
-        Err(error) => panic!("Problem opening the file: {:?}", error), // panic if any error occurred
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("test.txt") {
+                // if it is not exist handle it with creating a new file
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                // if there is other errors, ex. no access to read file, handle by this arm
+                panic!("Problem opening the file {:?}", other_error)
+            }
+        },
     };
+    // check unwrap and except to do this things in a more concise way
 }
