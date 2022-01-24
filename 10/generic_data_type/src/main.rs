752a213896da791ec599afa705a965f56ed7e02d
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-01-25 00:10:15 +0330

    define struct with generics

--- 10/generic_data_type/src/main.rs
+++ 10/generic_data_type/src/main.rs
@@ -11,6 +11,12 @@ fn largest<T>(list: &[T]) -> T {
     largest
 } // there will be an error, which we should learn traits to solve that
 
+// define a struct with two generic type
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
 fn main() {
     let number_list = vec![3, 5, 6, 1, 21, 4];
     let result = largest(&number_list);
@@ -19,4 +25,7 @@ fn main() {
     let char_list = vec!['v', 'e', 'c', 'd', 'a', 'u'];
     let result = largest(&char_list);
     println!("The largest char is {}", result);
+
+    let both_integer = Point { x: 5, y: 10 }; // in this situation we can use only one generic
+    let both_integer = Point { x: 5, y: 10.0 }; // in this situation we need both generic types
 }
