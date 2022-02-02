717beb36742d05957490d37c393f7fe5fc9f2edf
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-02-02 22:04:02 +0330

    define two scopes and represent lifetime of returned value by a function

--- 10/lifetimes/src/main.rs
+++ 10/lifetimes/src/main.rs
@@ -1,9 +1,14 @@
 fn main() {
     let string1 = String::from("abcd");
-    let string2 = "xyz";
-
-    let result = longest(string1.as_str(), string2);
-    println!("The longest string is \"{}\"", result);
+    {
+        let string2 = "xyz";
+        let result = longest(string1.as_str(), string2);
+        // if we try to print result out of this inner scope,
+        // compiler shows an error because the lifetime of string2 is not
+        // longer than this scope and result lifetime is equal to
+        // shorter lifetime of string1 and string 2
+        println!("The longest string is \"{}\"", result);
+    }
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
