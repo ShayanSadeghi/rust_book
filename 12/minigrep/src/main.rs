0c6cdfedf91851dcbd2c3907b3c74fb8de1e594a
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-02-10 23:08:33 +0330

    separating values passed as argument

--- 12/minigrep/src/main.rs
+++ 12/minigrep/src/main.rs
@@ -2,5 +2,10 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+
+    let query = &args[1]; // the first argument (args[0]) is the name of our binary
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
 }
