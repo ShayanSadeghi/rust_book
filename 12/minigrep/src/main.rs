2f41adf3e4ef4b297af94005f9ac1e676128213a
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-02-12 23:59:33 +0330

    init code refactoring

--- 12/minigrep/src/main.rs
+++ 12/minigrep/src/main.rs
@@ -3,10 +3,15 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
-    let query = &args[1]; // the first argument (args[0]) is the name of our binary
-    let filename = &args[2];
+    let (query, filename) = parse_config(&args);
 
     let contents = fs::read_to_string(filename).expect("Something went wrong during file reading");
     println!("Text is:\n{}", contents)
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
 }
