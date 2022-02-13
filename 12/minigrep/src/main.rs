286de876f78c4bd0f8db720b225366ba8095d14f
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-02-13 23:40:52 +0330

    added error handling

--- 12/minigrep/src/main.rs
+++ 12/minigrep/src/main.rs
@@ -1,21 +1,28 @@
 use std::env;
 use std::fs;
+use std::process;
 
 struct Config {
     query: String,
     filename: String,
 }
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("Not Enough Arguments!");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem passing arguments: {}", err);
+        process::exit(1);
+    });
 
     let contents =
         fs::read_to_string(config.filename).expect("Something went wrong during file reading");
