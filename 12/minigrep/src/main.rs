b84e2bff20f969eaf84741591559c2267b23b00a
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-02-13 23:22:34 +0330

    added cunstructor for Config

--- 12/minigrep/src/main.rs
+++ 12/minigrep/src/main.rs
@@ -5,19 +5,19 @@ struct Config {
     query: String,
     filename: String,
 }
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
 
+        Config { query, filename }
+    }
+}
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     let contents =
         fs::read_to_string(config.filename).expect("Something went wrong during file reading");
     println!("Text is:\n{}", contents)
-}
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-
-    Config { query, filename }
 }
