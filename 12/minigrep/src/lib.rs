4e8e1141e642da938e1cd08b11de8de763dbb7fe
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-02-15 21:38:38 +0330

    TDD init

--- 12/minigrep/src/lib.rs
+++ 12/minigrep/src/lib.rs
@@ -22,7 +22,25 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     //we just use run function for its side effect (Error)
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text\n{}", contents);
     Ok(())
 }
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive"], search(query, contents)); //TDD step 1: write a test that fails and check fail reason is what we design
+    }
+}
