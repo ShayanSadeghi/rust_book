09ad2b369114a48470976e1a3897bcd106297428
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-02-20 23:21:15 +0330

    use lazy evaluation

--- 13/closures/src/main.rs
+++ 13/closures/src/main.rs
@@ -1,23 +1,55 @@
 use std::thread;
 use std::time::Duration;
 
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
+
 fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_closure = |num| {
+    let mut expensive_result = Cacher::new(|num| {
         println!("calculation slowly...");
         thread::sleep(Duration::from_secs(2));
         num
-    };
+    });
 
     if intensity < 25 {
-        let expensive_res = expensive_closure(intensity);
-        println!("Today, Do {} pushups!", expensive_res);
-        println!("Then, Do {} situps", expensive_res);
+        println!("Today, Do {} pushups!", expensive_result.value(intensity));
+        println!("Then, Do {} situps", expensive_result.value(intensity));
     } else {
         if random_number == 3 {
             // now we don't call closure because we don't need that here
             println!("Take a break today! Remember to stay hydrated.");
         } else {
-            println!("Today, run for {} minutes", expensive_closure(intensity))
+            println!(
+                "Today, run for {} minutes",
+                expensive_result.value(intensity)
+            )
         }
     }
 }
