777af4a261cd8061ab4e539b97f8943e1c41fab8
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-02-21 23:18:47 +0330

    methods that consume the iterator

--- 13/iterators/src/main.rs
+++ 13/iterators/src/main.rs
@@ -6,4 +6,9 @@ fn main() {
     for val in v1_iter {
         println!("Got: {}", val);
     }
+
+    let v2 = vec![1, 2, 3]; // we consume v1_iter so we can't use it. we need to define a new iter
+    let v2_iter = v2.iter();
+    let total: i32 = v2_iter.sum(); // sum method is also a consuming adaptors so we can't use v2_iter after using that
+    println!("Total: {}", total);
 }
