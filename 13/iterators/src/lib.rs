677a380c9e3c9555b8a1c9ca0bd77dffdee89ee2
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-02-22 21:44:14 +0330

    using Iterator's next method

--- 13/iterators/src/lib.rs
+++ 13/iterators/src/lib.rs
@@ -1,20 +1,43 @@
 #[derive(PartialEq, Debug)]
-struct Shoe{
+struct Shoe {
     size: u32,
-    style: String
+    style: String,
 }
 
-fn shoes_in_size(shoes: Vec<Shoe>, shoe_size:u32)-> Vec<Shoe>{
+fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
     shoes.into_iter().filter(|s| s.size == shoe_size).collect()
 }
 
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32; //the iterator will return u32 values
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::*;
 
     #[test]
-    fn filters_by_size(){
-         let shoes = vec![
+    fn filters_by_size() {
+        let shoes = vec![
             Shoe {
                 size: 10,
                 style: String::from("sneaker"),
@@ -29,21 +52,31 @@ mod tests{
             },
         ];
 
-
         let in_my_size = shoes_in_size(shoes, 10);
 
         assert_eq!(
             in_my_size,
             vec![
-                Shoe{
-                    size:10,
+                Shoe {
+                    size: 10,
                     style: String::from("sneaker")
                 },
-                Shoe{
-                    size:10,
+                Shoe {
+                    size: 10,
                     style: String::from("boot")
                 }
             ]
-            );
+        );
+    }
+    #[test]
+    fn calling_next_directly() {
+        let mut counter = Counter::new();
+
+        assert_eq!(counter.next(), Some(1));
+        assert_eq!(counter.next(), Some(2));
+        assert_eq!(counter.next(), Some(3));
+        assert_eq!(counter.next(), Some(4));
+        assert_eq!(counter.next(), Some(5));
+        assert_eq!(counter.next(), None);
     }
 }
