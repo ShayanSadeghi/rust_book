25cf1f7f1fc2e69e0eeae337df9dda95e383d810
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-03-03 21:32:52 +0330

    use reference count to solve the previous problem

--- 15/reference_counting/src/main.rs
+++ 15/reference_counting/src/main.rs
@@ -1,12 +1,13 @@
 enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil,
 }
 
 use crate::List::{Cons, Nil};
+use std::rc::Rc;
 
 fn main() {
-    let a = Cons(5, Box::new(Cons(10, Box::new(Nil))));
-    let b = Cons(3, Box::new(a)); // here 'a' moved into b
-    let c = Cons(4, Box::new(a)); // here we want to use 'a' again but it's moved before
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    let b = Cons(3, Rc::clone(&a)); // Increase refrence count by 1
+    let c = Cons(4, Rc::clone(&a)); // Increase referece count by 1
 }
