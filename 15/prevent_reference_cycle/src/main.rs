be81b488c075d2576e00cdd97cd61ffe445e5a43
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-03-14 23:01:54 +0330

    compare weak and strong count

--- 15/prevent_reference_cycle/src/main.rs
+++ 15/prevent_reference_cycle/src/main.rs
@@ -19,13 +19,36 @@ fn main() {
         children: RefCell::new(vec![]),
     }); // leaf has no reference to branch
 
-    let branch = Rc::new(Node {
-        value: 5,
-        parent: RefCell::new(Weak::new()),
-        children: RefCell::new(vec![Rc::clone(&leaf)]),
-    }); // leaf is accessible through branch.children
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );
+    {
+        let branch = Rc::new(Node {
+            value: 5,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![Rc::clone(&leaf)]),
+        }); // leaf is accessible through branch.children
+            // use 'downgrade' to create a 'Weak<Node>' reference to branch
 
-    // use 'downgrade' to create a 'Weak<Node>' reference to branch
-    *leaf.parent.borrow_mut() = Rc::downgrade(&branch); // now child has access to parent
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch); // now child has access to parent
+        println!(
+            "branch strong = {}, weak = {}",
+            Rc::strong_count(&branch),
+            Rc::weak_count(&branch),
+        );
+
+        println!(
+            "leaf strong = {}, weak = {}",
+            Rc::strong_count(&leaf),
+            Rc::weak_count(&leaf),
+        );
+    }
     println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );
 }
