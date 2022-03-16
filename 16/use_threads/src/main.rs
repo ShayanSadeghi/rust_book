c5b2b9343b9e3534e11c851f96fd9b8749ffa398
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-03-16 21:46:26 +0330

    using move closures with threads

--- 16/use_threads/src/main.rs
+++ 16/use_threads/src/main.rs
@@ -3,8 +3,11 @@ use std::time::Duration;
 
 // to say rust wait until the spawn thread finish too, we use join
 fn main() {
-    let handle = thread::spawn(|| {
+    let v = vec![1, 2, 3]; // v is in the main thread
+                           // we use 'move' to force the closure to take the ownership of the values it's using rather than allowing rust to infer that it should borrow the values
+    let handle = thread::spawn(move || {
         // thread::spawn returns a JoinHandle
+        println!("the vector moved from main thread is: {:?}", v);
         for i in 1..10 {
             println!("Spawn number: {}", i);
             thread::sleep(Duration::from_millis(1));
@@ -18,6 +21,8 @@ fn main() {
         println!("Main number {}", i);
         thread::sleep(Duration::from_millis(1));
     }
+    // we can not run this, because we moved 'v' before
+    // println!("the vector in the main thread: {:?}", v);
 
     handle.join().unwrap(); // calling join on a JoinHandle tell rust to wait for its thread to finish
 }
