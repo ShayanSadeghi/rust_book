1b32374b4637fc823e59ea9a2c50f11fcd08e852
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-03-16 20:47:27 +0330

    use join to tell main thread wait for spawn thread

--- 16/use_threads/src/main.rs
+++ 16/use_threads/src/main.rs
@@ -1,18 +1,23 @@
 use std::thread;
 use std::time::Duration;
 
-// this code may have different output each time
+// to say rust wait until the spawn thread finish too, we use join
 fn main() {
-    thread::spawn(|| {
+    let handle = thread::spawn(|| {
+        // thread::spawn returns a JoinHandle
         for i in 1..10 {
             println!("Spawn number: {}", i);
             thread::sleep(Duration::from_millis(1));
         }
     });
 
+    // if we call handle.join here, rust will wait for the first thread to finish then continue the program(something like single thread)
+
     // program stopped when main thread ends
     for i in 1..5 {
         println!("Main number {}", i);
         thread::sleep(Duration::from_millis(1));
     }
+
+    handle.join().unwrap(); // calling join on a JoinHandle tell rust to wait for its thread to finish
 }
