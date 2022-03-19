cce1f37f27efa45d478d52b85973eb932feca303
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-03-19 14:52:52 +0330

    sharing a Mutex<T> between multiple threads

--- 16/share_state/src/main.rs
+++ 16/share_state/src/main.rs
@@ -1,12 +1,25 @@
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
+use std::thread;
+
+// as we used RefCell<T> to mutate contents inside a RC<T>, we use Mutex<T> to mutate contents inside an Arc<T>
 
 fn main() {
-    let m = Mutex::new(5); // Mutex<T> is a smart pointer
+    // we have to use Arc instead of Rc, because Arc is safe to use in concurrent situations
+    let counter = Arc::new(Mutex::new(0)); // Arc = atomic RC
+    let mut handles = vec![];
 
-    {
-        let mut num = m.lock().unwrap();
-        *num = 6;
-    } // lock release automatically
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap(); //mutex provides interior mutability
+            *num += 1;
+        });
+        handles.push(handle);
+    }
 
-    println!("m = {:?}", m);
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
