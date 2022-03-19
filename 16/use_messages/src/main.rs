fbb1a9813d2c36f47a3f76f332cf0201d57b9d00
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-03-19 13:41:46 +0330

    send multiple values to show threads communication

--- 16/use_messages/src/main.rs
+++ 16/use_messages/src/main.rs
@@ -1,15 +1,27 @@
 use std::sync::mpsc; // mpsc = multiple producer, single consumer
 use std::thread;
+use std::time::Duration;
 
 fn main() {
     let (tx, rx) = mpsc::channel(); //create a new channel --  transmitter(tx), receiver(rx)
 
     thread::spawn(move || {
         // create a new thread
-        let val = String::from("hi");
-        tx.send(val).unwrap(); // send method returns a Result<T, E>
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap(); // send method returns a Result<T, E>
+            thread::sleep(Duration::from_secs(1));
+        }
     });
 
-    let recieved = rx.recv().unwrap(); // wait until a value is sent down the channel
-    println!("Got: {}", recieved);
+    for recieved in rx {
+        // let recieved = rx.recv().unwrap(); // wait until a value is sent down the channel
+        println!("Got: {}", recieved);
+    }
 }
