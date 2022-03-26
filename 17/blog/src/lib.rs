9534995c1022e4eb958498947dd015394b43554a
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-03-26 20:43:01 +0430

    Requesting a Review of the post changes its state

--- 17/blog/src/lib.rs
+++ 17/blog/src/lib.rs
@@ -16,10 +16,31 @@ impl Post {
     pub fn content(&self) -> &str {
         "" // it's a placeholder
     }
+
+    pub fn request_review(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.request_review())
+        }
+    }
 }
 
-trait State {}
+trait State {
+    fn request_review(self: Box<Self>) -> Box<dyn State>; // first parameter is Box<Self> so this method takes ownership of Box<Self>,...
+                                                          // invalidating the old state and then the state value of Post can transform into a new state
+}
 
 struct Draft {}
 
-impl State for Draft {}
+impl State for Draft {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        Box::new(PendingReview {})
+    }
+}
+
+struct PendingReview {}
+
+impl State for PendingReview {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self // there is no change in state
+    }
+}
