07c4f18ec43bedd968f18ae7ea8e024ca36b51ba
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-03-27 23:36:53 +0430

    state pattern completed

--- 17/blog/src/lib.rs
+++ 17/blog/src/lib.rs
@@ -14,7 +14,7 @@ impl Post {
         self.content.push_str(text); // adding text to content does not related to the 'state' of the Post
     }
     pub fn content(&self) -> &str {
-        "" // it's a placeholder
+        self.state.as_ref().unwrap().content(self)
     }
 
     pub fn request_review(&mut self) {
@@ -22,11 +22,24 @@ impl Post {
             self.state = Some(s.request_review())
         }
     }
+
+    pub fn approve(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.approve())
+        }
+    }
 }
 
 trait State {
     fn request_review(self: Box<Self>) -> Box<dyn State>; // first parameter is Box<Self> so this method takes ownership of Box<Self>,...
                                                           // invalidating the old state and then the state value of Post can transform into a new state
+
+    fn approve(self: Box<Self>) -> Box<dyn State>;
+
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        // a default implementation for the 'content', so we don't need to implement 'content' in the 'Draft' and 'PendingReview'
+        ""
+    }
 }
 
 struct Draft {}
@@ -35,6 +48,10 @@ impl State for Draft {
     fn request_review(self: Box<Self>) -> Box<dyn State> {
         Box::new(PendingReview {})
     }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
 }
 
 struct PendingReview {}
@@ -42,5 +59,24 @@ struct PendingReview {}
 impl State for PendingReview {
     fn request_review(self: Box<Self>) -> Box<dyn State> {
         self // there is no change in state
+    }
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Published {})
+    }
+}
+
+struct Published {}
+
+impl State for Published {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        &post.content
     }
 }
