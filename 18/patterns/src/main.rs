831f8e53ce0186321f8bfe8cd862aebcdd94996e
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-04-04 14:35:11 +0430

    Patterns -- section 18.1

---
+++ 18/patterns/src/main.rs
@@ -0,0 +1,45 @@
+fn main() {
+    // (if let)-(else if let) example to set a background color
+    let favorite_color: Option<&str> = None;
+    let is_tuesday = false;
+    let age: Result<u8, _> = "34".parse();
+
+    if let Some(color) = favorite_color {
+        println!("favorite color is activated, color: {}", color);
+    } else if is_tuesday {
+        println!("Tuesday is green day")
+    } else if let Ok(age) = age {
+        // "age" is a shadow variable, which we define just now
+        if age > 30 {
+            println!("using purple")
+        } else {
+            println!("using orange")
+        }
+    } else {
+        // (if let) is not exhaustive, so if we forget this last (else) compiler wouldn't alert us, unlike the (match)
+        println!("using blue")
+    }
+
+    // while let -- print in reverse order
+    let mut stack = Vec::new();
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    while let Some(top) = stack.pop() {
+        // if stack.pop returns a (None), while loop will stop
+        println!("{}", top)
+    }
+
+    // for
+    let v = vec!['a', 'b', 'c'];
+    for (index, value) in v.iter().enumerate() {
+        println!("{} is at index {}", value, index);
+    }
+
+    // let--- let is a pattern!!! :))
+    // let PATTERN = EXPRESSION
+    let x = 5;
+    let (x, y, z) = (1, 2, 3); // it's also a pattern
+                               //function parameters are patterns too
+}
