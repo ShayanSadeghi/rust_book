3e318c1cf1133db040b16ea59dedde55fe1cb8a3
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-04-11 21:39:44 +0430

    make a size check for new function in ThreadPool

--- 20/server/src/lib.rs
+++ 20/server/src/lib.rs
@@ -1,7 +1,15 @@
 pub struct ThreadPool;
 
 impl ThreadPool {
+    /// Create a new ThreadPool
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    /// The `new` function will panic if the size is zero.
     pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0); // usize can accept non-negative integers, but we don't want 0 too,
+                           //so we do this to panic when a user want to create a ThreadPool with 0 thread
         ThreadPool
     }
 
