9b950ab30fece49152a1a34beb3827dd1a99591f
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-04-12 21:40:13 +0430

    added a space to store threads

--- 20/server/src/lib.rs
+++ 20/server/src/lib.rs
@@ -1,4 +1,8 @@
-pub struct ThreadPool;
+use std::thread;
+
+pub struct ThreadPool {
+    threads: Vec<thread::JoinHandle<()>>,
+}
 
 impl ThreadPool {
     /// Create a new ThreadPool
@@ -10,7 +14,14 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0); // usize can accept non-negative integers, but we don't want 0 too,
                            //so we do this to panic when a user want to create a ThreadPool with 0 thread
-        ThreadPool
+
+        let mut threads = Vec::with_capacity(size); // "with_capacity" is like "new", but the space is preallocated
+
+        for _ in 0..size {
+            // create some threads and store them in the vector
+        }
+
+        ThreadPool { threads }
     }
 
     pub fn execute<F>(&self, f: F)
