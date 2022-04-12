b8dd28086dbfbdb58558f894a385236c23717101
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-04-12 22:08:05 +0430

    added Worker struct

--- 20/server/src/lib.rs
+++ 20/server/src/lib.rs
@@ -1,7 +1,7 @@
 use std::thread;
 
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
 }
 
 impl ThreadPool {
@@ -15,18 +15,30 @@ impl ThreadPool {
         assert!(size > 0); // usize can accept non-negative integers, but we don't want 0 too,
                            //so we do this to panic when a user want to create a ThreadPool with 0 thread
 
-        let mut threads = Vec::with_capacity(size); // "with_capacity" is like "new", but the space is preallocated
+        let mut workers = Vec::with_capacity(size); // "with_capacity" is like "new", but the space is preallocated
 
-        for _ in 0..size {
-            // create some threads and store them in the vector
+        for id in 0..size {
+            workers.push(Worker::new(id));
         }
 
-        ThreadPool { threads }
+        ThreadPool { workers }
     }
 
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+        Worker { id, thread }
     }
 }
